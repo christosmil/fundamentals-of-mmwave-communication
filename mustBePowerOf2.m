f7b4e2f504a03b893a5112724f6fd295cdeb901a
christosmil <48249186+christosmil@users.noreply.github.com>
2024-10-25 19:03:06 +0300

    deprecate mustBePowerOf2.m

--- mustBePowerOf2.m
+++ mustBePowerOf2.m
@@ -7,6 +7,14 @@ function mustBePowerOf2(numbers)
 %
 % The function throws an error if any number of the array is not a power of
 % 2.
+%
+% --
+% [DEPRECATED] The efficient way to do this is to either use bitwise and
+% operation between the target number and the previous or count the bits
+% of the target number. Links:
+% https://www.mathworks.com/help/matlab/matlab_prog/bit-wise-operations.html
+% https://x.com/raymondh/status/1694763110723187012
+
 
 % Check if all inputs is power of 2.
 numbers = double(numbers);
