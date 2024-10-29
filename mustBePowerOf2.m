224fa00ade3238ed555960fa8cc511427745f501
christosmil <48249186+christosmil@users.noreply.github.com>
2024-10-29 14:46:43 +0200

    Update mustBePowerOf2.m

--- mustBePowerOf2.m
+++ mustBePowerOf2.m
@@ -8,10 +8,12 @@ function mustBePowerOf2(numbers)
 % The function throws an error if any number of the array is not a power of
 % 2.
 
-% Check if all inputs is power of 2.
+% This is needed for the beam_gain_pattern.m
 numbers = double(numbers);
+
+% Check if all inputs is power of 2.
 for iter=1:length(numbers) 
-    if sum(bitget(numbers(iter), 1:64))
+    if sum(bitget(numbers(iter), 1:64)) ~= 1
         error('Value must be a positive power of 2.');
     end
 end
